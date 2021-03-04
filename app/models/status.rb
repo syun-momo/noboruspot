1649ecec64ddf81b672d50876456bfc6fde50940
syun-momo <72676307+syun-momo@users.noreply.github.com>
2021-03-04 22:13:38 +0900

    プルダウン記載

---
+++ app/models/status.rb
@@ -0,0 +1,9 @@
+class Status < ctiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '屋内' },
+    { id: 3, name: '屋外' },
+  ]
+  include ActiveHash::Associations
+  has_many :items
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :Users_items
+  has_many :Items, through: :Users_items
+
+  with_options presence: true do
+    validates :nickname
+  end
 end
